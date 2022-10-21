d0286028e0c9305daebd69d0f6360226399458e5
unknown <cesarrcosta99@gmail.com>
2022-10-21 17:50:23 -0300

    Atividade Feita

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+  // Exercicio 1
+let par=Number(prompt("insira um numero par"))
+let restoDivisao= par % 2
+console.log("o resto da divisão foi:" , restoDivisao)
+   // o padrão é que toda divisão de um numero par por 2 o resto será 0,ou seja, é uma divisão exata
+   // O padrão é que toda divisão de um numero impar por 2 o resto será 1,ou seja,não é uma divisão exata
+
+   
+
